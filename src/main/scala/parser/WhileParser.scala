e64ff9872fa8845b605f5802da3000f41ac61dfd
Calavoow <Calavoow@users.noreply.github.com>
2014-09-20 18:15:43 +0200

    Completed While language
    Added left-associativity to all operators, but no operator precedence.

--- src/main/scala/parser/WhileParser.scala
+++ src/main/scala/parser/WhileParser.scala
@@ -13,60 +13,83 @@ object WhileParser extends RegexParsers {
 	def op_r = """<|>|==""".r ^^ {_.toString}
 	def op_b = """&&|\|\|""".r ^^ {_.toString}
 
-//	def exp : Parser[Expression] = (integer ^^ {INT(_)}) | (exp ~ op ~ exp ^^ {
-//		case e1 ~ operator ~ e2 => BinOp(operator, e1, e2)
-//	})
+	def aExp: Parser[AExp] = term ~ (op_a ~ term).* ^^ mkTreeArithmatic
 
-	def aExp: Parser[AExp] = term ~ (
-		op_a ~ aExp
-	  ).? ^^ {
-		case f1 ~ Some(op ~ f2) => BinOp(op, f1, f2)
-		case f1 ~ None => f1
-	}
 	def term : Parser[AExp] = (
 		("(" ~> aExp <~ ")")
 		| (identifier ^^ Ref)
 		| (integer ^^ INT)
 	)
 
+  def mkTreeArithmatic(input: AExp ~ List[String ~ AExp]): AExp = {
+    def combine(acc: AExp, next: String ~ AExp) = {
+      next match {
+        case op ~ y => BinOp(op, acc, y)
+      }
+    }
+
+    input match {
+      case first ~ rest => ((first: AExp) /: rest)(combine)
+    }
+  }
+
 	def bExp: Parser[BExp] = (
-		binValue ~ (op_b ~ bExp).? ^^ {
-			case b1 ~ Some(op ~ b2) => BinBExp(op, b1, b2)
-			case b1 ~ None => b1
-		}
+		binTerm ~ (op_b ~ binTerm).* ^^ mkTreeBoolean
 		| "not" ~> bExp ^^ {case e => Not(e)}
-		| aExp ~ op_r ~ aExp ^^ { case e1 ~ op ~ e2 => RelationalExp(op, e1, e2)}
 	)
 
-	def binValue = "true" ^^ {case _ => True} | "false" ^^ {case _ => False}
+	def binTerm = (
+      "true" ^^ {case _ => True}
+      | "false" ^^ {case _ => False}
+      | aExp ~ op_r ~ aExp ^^ { case e1 ~ op ~ e2 => RelationalExp(op, e1, e2)}
+  )
+
+  def mkTreeBoolean(input: BExp ~ List[String ~ BExp]): BExp = {
+    def combine(acc: BExp, next: String ~ BExp) = {
+      next match {
+        case op ~ y => BinBExp(op, acc, y)
+      }
+    }
+
+    input match {
+      case first ~ rest => ((first: BExp) /: rest)(combine)
+    }
+  }
 
 
-	def statements = statement*
+	def statements = statement.*
 	def block = "{" ~> statements <~ "}" ^^ {l => Block(l)}
-	def statement : Parser[Statement] = whileLoop | block | assignment
+	def statement : Parser[Statement] = whileLoop | ifelse | assignment | skip | block
+
+  def whileLoop = ("while(" ~> bExp <~ ")" ) ~ statement ^^
+    { case conditional ~ body => While(conditional, body) }
 
 	def assignment = (identifier <~ ":=") ~ aExp ^^ {
 		case id ~ e => Assig(id, e)
 	}
-	def whileLoop = ("while(" ~> bExp <~ ")" ) ~ statement ^^
-	  { case conditional ~ body => While(conditional, body) }
 
+  def ifelse = ("if" ~> bExp) ~ ("then" ~> statement) ~ ("else" ~> statement) ^^ {
+    case bexp ~ s1 ~ s2 => IfElse(bexp, s1, s2)
+  }
+
+  def skip = "skip" ^^ {case _ => Skip}
 }
 
-abstract trait BExp
+trait BExp
 case object True extends BExp
 case object False extends BExp
 case class Not(e1: BExp) extends BExp
 case class BinBExp(operator: String, e1: BExp, e2: BExp) extends BExp
 case class RelationalExp(operator: String, e1: AExp, e2: AExp) extends BExp
 
-abstract trait AExp
+trait AExp
 case class BinOp(operator: String, e1: AExp, e2: AExp) extends AExp
 case class INT(value : Int) extends AExp
 case class Ref(id: String) extends AExp
 
-abstract trait Statement
+trait Statement
 case class Block(statements: List[Statement]) extends Statement
 case class While(conditional: BExp, stmt: Statement) extends Statement
 case class Assig(ref: String, exp: AExp) extends Statement
 case class IfElse(condition: BExp, s1: Statement, s2: Statement) extends Statement
+case object Skip extends Statement
